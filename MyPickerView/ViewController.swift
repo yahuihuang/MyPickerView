47de8eea374c70a465dc900137c953cdd53dcef1
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-23 15:58:10 +0800

    add blood and astrological

--- MyPickerView/ViewController.swift
+++ MyPickerView/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     @IBOutlet weak var myPickerView: UIPickerView!
     
+    var astrological = ["請選擇你的星座","白羊宮","金牛宮","雙子宮","巨蟹 宮","獅子宮","處女宮","天秤宮","天蠍宮","射手宮","摩羯宮","水瓶宮","雙魚 宮"]
+    var bloudType = ["請選擇你的血型","A","B","O","AB"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // https://developer.apple.com/documentation/uikit/uipickerviewdatasource
@@ -21,15 +24,29 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     //MARK:Picker View Data Source & Delegate
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 3
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+        switch component {
+        case 0:
+            return bloudType.count
+        case 1:
+            return astrological.count
+        default:
+            return 0
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "Hello"
+        switch component {
+        case 0:
+            return bloudType[row]
+        case 1:
+            return astrological[row]
+        default:
+            return ""
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
