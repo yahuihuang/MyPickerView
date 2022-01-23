f837ea71bdc6765bd1a08b060cd5708b46d117eb
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-23 16:38:22 +0800

    Segue

--- MyPickerView/ViewController.swift
+++ MyPickerView/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     var astrological = ["請選擇你的星座","白羊宮","金牛宮","雙子宮","巨蟹 宮","獅子宮","處女宮","天秤宮","天蠍宮","射手宮","摩羯宮","水瓶宮","雙魚 宮"]
     var bloudType = ["請選擇你的血型","A","B","O","AB"]
     
+    @IBOutlet weak var lblNew: UILabel!
+    @IBOutlet weak var lblOne: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // https://developer.apple.com/documentation/uikit/uipickerviewdatasource
@@ -21,7 +24,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         myPickerView.dataSource = self
         myPickerView.delegate = self
     }
-
     //MARK:Picker View Data Source & Delegate
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
@@ -50,7 +52,23 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("didSelectedRow: \(row)\tinComponent: \(component)")
+        if row == 0 {
+            return
+        }
+        
+        switch component {
+        case 0:
+            lblOne.text = bloudType[row]
+        case 1:
+            lblNew.text = astrological[row]
+        default:
+            break;
+        }
+        
+        if pickerView.selectedRow(inComponent: 0) != 0 &&
+            pickerView.selectedRow(inComponent: 1) != 0 {
+            self.performSegue(withIdentifier: "goNextPage", sender: nil)
+        }
     }
 }
 
