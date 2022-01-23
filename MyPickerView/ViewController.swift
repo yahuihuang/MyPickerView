c8eaad8568ccfdedaa0de2f9deb67b5df86cb811
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-23 15:37:55 +0800

    UIPickerView

--- MyPickerView/ViewController.swift
+++ MyPickerView/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // https://developer.apple.com/documentation/uikit/uipickerviewdatasource
+        // UIPickerViewDataSource is protocol
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
     }
 
-
+    //MARK:Picker View Data Source & Delegate
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "Hello"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("didSelectedRow: \(row)\tinComponent: \(component)")
+    }
 }
 
